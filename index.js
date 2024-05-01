0765629aac0f7e2d64799b0e8427493b147ed65b
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2024-04-30 20:12:25 -0400

    Restarted index.js
    - app is working as expected

--- index.js
+++ index.js
@@ -3,42 +3,44 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+let shortUrlMap = []
+
+console.log('**** Hello URL Shortener! ****')
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-/* SIMPLE LOGGER */
-app.use("/", express.static(__dirname + "/"),
-    function(req, res, next){
-    console.log(req.method + " " + req.path + " - " + req.ip)
-    next()
-    }
-)
-
 /* BODY PARSER */
-app.use("/", bodyParser.urlencoded({extended: false}))
+app.use('/', bodyParser.urlencoded({ extended: false }))
 
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// Your first API endpoint
-app.use('/api/shorturl', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+//POST to /api/shorturl
+app.post('/api/shorturl', function(req, res) {
+	console.log({ original_url : req.body.url, short_url : 1})
 
+	//See if URL is parsable
+
+
+	//send back the JSON
+  res.json({ original_url : req.body.url, short_url : 1});
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
