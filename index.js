61318b4db916107264a3e3ea057c8f26ba9b9b8d
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2024-04-30 21:21:00 -0400

    Completed challenge
    - passes FCC tests!

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
 const bodyParser = require('body-parser')
 const dns = require('dns')
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-let shortUrlMap = []
+const shortUrlArray = []
 
 console.log('**** Hello URL Shortener! ****')
 
-app.use(cors());
+app.use(cors())
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(`${process.cwd()}/public`))
 
 /* BODY PARSER */
 app.use('/', bodyParser.urlencoded({ extended: false }))
 
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-//POST to /api/shorturl
-app.post('/api/shorturl', function(req, res) {
-	console.log({ original_url : req.body.url, short_url : 1})
+// handle GET requests to /api/shorturl/:shorturl
+app.get('/api/shorturl/:shorturl', function (req, res) {
+  const index = req.params.shorturl
+  console.log({ Shorturl_index: index})
 
-	//See if URL is parsable
+  // use 'index' to find original url in map
+  const redirectUrl = shortUrlArray[index]
+  // redirect to original url
+  console.log({ redirecting_to: redirectUrl })
+  res.redirect(redirectUrl)
+})
 
+// POST to /api/shorturl
+app.post('/api/shorturl', function (req, res) {
+  let parsedUrl = ''
+  const originalUrl = req.body.url
+  // See if URL is parsable
+  if (URL.canParse(originalUrl)) {
+    // If it can be parsed,
+    // - parse it
+    parsedUrl = new URL(originalUrl)
 
-	//send back the JSON
-  res.json({ original_url : req.body.url, short_url : 1});
-});
+    // then check if it is a valid url
+    dns.lookup(parsedUrl.hostname, (error, address, family) => {
+      if (error) {
+        	// if it breaks
+        	console.log({ error: 'invalid url' })
+        	res.json({ error: 'invalid url' })
+      } else {
+        	// proceed with adding the url to an array
+        shortUrlArray.push(originalUrl)
+        console.log('Array length: ', shortUrlArray.length)
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+        console.log({ original_url: req.body.url, short_url: 1 })
+        res.json({ original_url: req.body.url, short_url: 1 })
+      }
+    })
+  }
 
+  // send back the JSON
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
